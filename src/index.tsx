5ab6d4e75f2cd2012ee528ddc8db980d31738a6d
Lelberto <jsurieux@gmail.com>
2022-05-30 20:11:19 +0200

    Init TailwindCSS

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
+import './css/index.css';
 import reportWebVitals from './report-web-vitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
