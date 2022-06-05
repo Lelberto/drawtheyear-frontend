60bdcad7ac2f5503a1f61b85bec02ebb1cc47bec
Lelberto <jsurieux@gmail.com>
2022-06-05 21:59:00 +0200

    Add useDay and update useDays

--- src/hooks/day.ts
+++ src/hooks/day.ts
@@ -1,23 +1,25 @@
+import moment from 'moment';
 import { useEffect, useState } from 'react';
 import { Day } from '../types/data';
 import { Response } from '../types/responses';
 import { useQuery } from './query';
 
 /**
- * Days hook
+ * Day hook
  * 
  * @param userId User id
+ * @param date Day date
  * @returns User days
  */
-export const useDays = (userId: string) => {
-  const [days, setDays] = useState<Day[]>([]);
+export const useDay = (userId: string, date: Date) => {
+  const [day, setDay] = useState<Day>();
   const query = useQuery();
 
   useEffect(() => {
-    query.get<Response>(`http://localhost:8080/users/${userId}/days`)
-      .then((res) => setDays(res.data.days))
+    query.get<Response>(`http://localhost:8080/users/${userId}/days/${moment(date).format('YYYY-MM-DD')}`)
+      .then((res) => setDay(res.data.day))
       .catch(console.error);
-  }, [userId, query]);
+  }, [userId, date, query]);
 
-  return days;
+  return day;
 }
