01d2790c413f9c3d323a84df360fa0ecf1898fe5
Lelberto <jsurieux@gmail.com>
2022-06-17 23:05:30 +0200

    Add entry point and users responses

--- src/types/responses.ts
+++ src/types/responses.ts
@@ -1,6 +1,21 @@
 /** API response types */
 
+import { User } from './data';
+import { Hateoas } from './hateoas';
+
 /** Base response */
 export type Response = {
   data: any;
+}
+
+/** Entry point response */
+export type EntryPointResponse = {
+  data: Hateoas;
+}
+
+/** Users response */
+export type UsersResponse = {
+  data: {
+    users: User[]
+  };
 }
