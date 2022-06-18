af5fea973d0327408e66f714d4d0687301b992da
Lelberto <jsurieux@gmail.com>
2022-06-18 14:42:30 +0200

    Update CSS and Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         schoolwork: ['Schoolwork', 'sans-serif']
+      },
+      colors: {
+        primary: '#845EC2',
+        secondary: '#FFC75F'
       }
     },
   },
