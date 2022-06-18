078068999865a7ca432f7d2fc1ba6d2998753f99
Lelberto <jsurieux@gmail.com>
2022-06-18 14:30:51 +0200

    Add assets folder Move CSS into assets Add fonts folder Add Schoolwork font Add some CSS rules

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app';
-import './css/index.css';
+import './assets/css/index.css';
 import reportWebVitals from './report-web-vitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
