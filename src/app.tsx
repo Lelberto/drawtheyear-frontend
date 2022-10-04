82a8f7d68b941022ddd6f9772e12523856edf2ea
Lelberto <jsurieux@gmail.com>
2022-10-04 19:58:19 +0200

    Add Tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <></>
+    <h1 className="text-5xl text-red-700">Hello World !</h1>
   );
 }
 
