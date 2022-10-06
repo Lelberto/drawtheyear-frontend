b1d6d9c73d7580df5c1023354cfb179c4f984ef7
Lelberto <jsurieux@gmail.com>
2022-10-06 23:52:50 +0200

    Add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './app';
 import reportWebVitals from './reportWebVitals';
+import { router } from './routes/router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/routes/routes.tsx
@@ -0,0 +1,31 @@
+import { RouteObject } from 'react-router-dom';
+import { App } from '../app';
+import { LoginCallbackView } from '../views/login/login-callback.view';
+import { LoginFailureView } from '../views/login/login-failure.view';
+import { LoginSuccessView } from '../views/login/login-success.view';
+import { LoginView } from '../views/login/login.view';
+
+export const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'login',
+        element: <LoginView />
+      },
+      {
+        path: 'login/callback',
+        element: <LoginCallbackView />
+      },
+      {
+        path: 'login/success',
+        element: <LoginSuccessView />
+      },
+      {
+        path: 'login/failure',
+        element: <LoginFailureView />
+      }
+    ]
+  }
+];
