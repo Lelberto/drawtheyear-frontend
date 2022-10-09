61f0c64c587137e7e5ba4340bb3872abb9200e22
Miki Namsrai <miki.namsrai@gmail.com>
2022-10-09 16:59:42 +0200

    Navbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/navbar";
 
-function App() {
-  return (
-    <h1 className="text-5xl text-red-700">Hello World !</h1>
-  );
+export function App() {
+    return (
+        <div className="bg-bgdty-dark">
+            <Navbar />
+            <Outlet />
+        </div>
+    );
 }
-
-export default App;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <div>
+                <h1 className="text-3xl font-thin font-avengers text-[#007f97] p-5 mb-4">
+                    Draw The Year
+                </h1>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
