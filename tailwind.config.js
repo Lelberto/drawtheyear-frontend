3d56acd57d2fc0c4b5c4387032ac7c1ed9b8946e
Miki Namsrai <miki.namsrai@gmail.com>
2022-10-09 16:59:13 +0200

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                bgdty: {
+                    light: "#a4bcbc",
+                    DEFAULT: "#FFF",
+                    dark: "#051126",
+                },
+            },
+            fontFamily: {
+                body: ['"Quick Sand"'],
+            },
+        },
+    },
+    plugins: [],
+};
