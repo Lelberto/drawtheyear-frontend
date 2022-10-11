981b79f2b03c8c1661467344929e389a147139c0
Lelberto <jsurieux@gmail.com>
2022-10-11 21:34:36 +0200

    Add theme colors and Roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto-Regular', 'sans-serif']
+      },
+      colors: {
+        'primary': '#8758FF',
+        'secondary': '#5CB8E4',
+        'light': '#F2F2F2',
+        'dark': '#181818'
+      }
+    },
   },
   plugins: [],
 }
