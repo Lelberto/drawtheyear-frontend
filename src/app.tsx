0ec0653251869a2a0baff3ac82e4c7b7d326b321
Lelberto <jsurieux@gmail.com>
2022-10-18 17:05:02 +0200

    Add container

--- src/app.tsx
+++ src/app.tsx
@@ -4,13 +4,15 @@ import { AuthProvider } from './contexts/auth.context';
 
 export const App = () => (
   <AuthProvider>
-    <div className="sticky top-0 z-40 backdrop-blur">
-      <Navbar />
-    </div>
-    {/* Test section */}
-    {/* Test section */}
-    <div className="m-8">
-      <Outlet />
+    <div className="md:container md:mx-auto">
+      <div className="sticky top-0 z-40 backdrop-blur">
+        <Navbar />
+      </div>
+      {/* Test section */}
+      {/* Test section */}
+      <div className="m-8">
+        <Outlet />
+      </div>
     </div>
   </AuthProvider>
 );
