60f4f33dda893870eae582cc27ea677b5eed7437
Miki Namsrai <miki.namsrai@gmail.com>
2022-11-06 21:27:43 +0100

    fix bug #1
    Fixed by changing some shit
    
    #1

--- src/components/buttons/visibility.button.tsx
+++ src/components/buttons/visibility.button.tsx
@@ -1,41 +1,47 @@
-import { faLock, faUnlock } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import classNames from 'classnames';
-import { ButtonHTMLAttributes, useMemo } from 'react';
-import { Day } from '../../types/data.types';
+import { faLock, faUnlock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
+import { ButtonHTMLAttributes, useMemo } from "react";
+import { Day } from "../../types/data.types";
 
 export type VisibilityButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  visibility?: Day['visibility'];
-}
-export const VisibilityButton = ({ className, visibility = 'private', ...props }: VisibilityButtonProps) => {
-  const iconElement = useMemo(() => {
-    switch (visibility) {
-      case 'private':
-      default: return (<FontAwesomeIcon icon={faUnlock} />);
-      case 'public': return (<FontAwesomeIcon icon={faLock} />);
-    }
-  }, [visibility]);
+    visibility?: Day["visibility"];
+};
+export const VisibilityButton = ({
+    className,
+    visibility = "private",
+    ...props
+}: VisibilityButtonProps) => {
+    const iconElement = useMemo(() => {
+        switch (visibility) {
+            case "private":
+            default:
+                return <FontAwesomeIcon icon={faUnlock} />;
+            case "public":
+                return <FontAwesomeIcon icon={faLock} />;
+        }
+    }, [visibility]);
 
-  className = classNames(
-    'flex justify-center',
-    'w-10 h-10 p-2',
-    'bg-dark',
-    'border rounded-full',
-    {
-      'border-green-500': visibility === 'public',
-      'border-red-500': visibility === 'private'
-    },
-    'text-xl',
-    {
-      'text-green-500': visibility === 'public',
-      'text-red-500': visibility === 'private'
-    },
-    className
-  );
+    className = classNames(
+        "flex justify-center",
+        "w-10 h-10 p-2",
+        "bg-dark",
+        "border rounded-full",
+        {
+            "border-red-500": visibility === "public",
+            "border-green-500": visibility === "private",
+        },
+        "text-xl",
+        {
+            "text-red-500": visibility === "public",
+            "text-green-500": visibility === "private",
+        },
+        className
+    );
 
-  return (
-    <button className={className} {...props}>
-      {iconElement}
-    </button>
-  );
-}
+    return (
+        <button className={className} {...props}>
+            {iconElement}
+        </button>
+    );
+};
