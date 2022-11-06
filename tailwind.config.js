9e403644116ba0d890d3657d837c7bd445057a38
Miki Namsrai <miki.namsrai@gmail.com>
2022-11-06 21:39:18 +0100

    Add Quicksand font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'roboto': ['Roboto-Regular', 'sans-serif']
-      },
-      maxHeight: {
-        '128': '25.6rem'
-      },
-      colors: {
-        'primary': '#8758FF',
-        'secondary': '#5CB8E4',
-        'light': '#F2F2F2',
-        'dark': '#181818'
-      }
-    }
-  },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'roboto': ['Roboto-Regular', 'sans-serif'],
+                'quicksand': ['Quicksand', 'sans-serif'],
+            },
+            maxHeight: {
+                '128': '25.6rem'
+            },
+            colors: {
+                'primary': '#8758FF',
+                'secondary': '#5CB8E4',
+                'light': '#F2F2F2',
+                'dark': '#181818'
+            }
+        }
+    },
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
 }
